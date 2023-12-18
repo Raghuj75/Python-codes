c64ca1a776550c59c9c18932232ce9e4fe7e4b41
Raghuj75 <153974430+Raghuj75@users.noreply.github.com>
2023-12-18 13:21:57 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+print("Calculator")
+print("1.Add")
+print("2.Subtract")
+print("3.multiply")
+print("4.divide")
+x=int(input("enter the value of x"))
+y=int(input("enter the value of y"))
+z=input("enter the value")
+if z=='Add':
+    print(x+y)
+if z=='Subtract':
+    print(x-y)
+if z=='multiply':
+    print(x*y)
+if z=='divide':
+    print(x/y)
